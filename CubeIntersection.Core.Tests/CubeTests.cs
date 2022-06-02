e32a064db458f91967b0330c82f47d97fb738ba7
Arnau Lacambra <naulacambra@gmail.com>
2022-06-02 19:31:28 +0200

    Added more cube tests

--- CubeIntersection.Core.Tests/CubeTests.cs
+++ CubeIntersection.Core.Tests/CubeTests.cs
@@ -73,6 +73,36 @@ namespace CubeIntersection.Core.Tests
         }
 
         [Fact]
+        public void Cube_Collides_In_X_And_Y_Axis()
+        {
+            var a = new Cube(0, 0, 0, 10, 10, 10);
+            var b = new Cube(5, 5, 0, 10, 10, 10);
+
+            Assert.True(a.Collides(b));
+            Assert.True(b.Collides(a));
+        }
+
+        [Fact]
+        public void Cube_Collides_In_X_And_Y_And_Z_Axis()
+        {
+            var a = new Cube(0, 0, 0, 10, 10, 10);
+            var b = new Cube(5, 5, 5, 10, 10, 10);
+
+            Assert.True(a.Collides(b));
+            Assert.True(b.Collides(a));
+        }
+
+        [Fact]
+        public void Cube_Not_Collides_In_X_And_Y_And_Z_Axis()
+        {
+            var a = new Cube(0, 0, 0, 10, 10, 10);
+            var b = new Cube(5, 5, 20, 10, 10, 10);
+
+            Assert.False(a.Collides(b));
+            Assert.False(b.Collides(a));
+        }
+
+        [Fact]
         public void Cube_Not_Collides_Adjacent_In_X_Axis()
         {
             var a = new Cube(0, 0, 0, 10, 10, 10);
