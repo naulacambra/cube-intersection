fbf28583f06d34fba84ce67ebd53534c347e19b2
Arnau Lacambra <naulacambra@gmail.com>
2022-06-02 17:04:50 +0200

    First commit

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Cube instersection");
+Console.WriteLine("1st body");
+Console.Write("Position (x, y, z): ");
+var position = Console.ReadLine();
+
+Console.WriteLine(position);
