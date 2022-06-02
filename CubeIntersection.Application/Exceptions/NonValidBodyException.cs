99a8c98a01f9e88a05dced9c3360abcb49c2afab
Arnau Lacambra <naulacambra@gmail.com>
2022-06-02 18:19:57 +0200

    Created specific exception for colliders when checking bodies

---
+++ CubeIntersection.Application/Exceptions/NonValidBodyException.cs
@@ -0,0 +1,15 @@
+﻿namespace CubeIntersection.Application.Exceptions
+{
+    public class NonValidBodyException : Exception
+    {
+        public NonValidBodyException() : base()
+        {
+
+        }
+
+        public NonValidBodyException(string message) : base(message)
+        {
+
+        }
+    }
+}
