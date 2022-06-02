7f04018245300d1ce7ca385f2f4ff0730356e4b9
Arnau Lacambra <naulacambra@gmail.com>
2022-06-02 18:58:32 +0200

    Added comments to body entity

--- CubeIntersection.Core/Entities/Body.cs
+++ CubeIntersection.Core/Entities/Body.cs
@@ -12,17 +12,17 @@ namespace CubeIntersection.Core.Entities
 
         public Body(string position, string size)
         {
-            ArgumentNullException.ThrowIfNull(position, "Position");
-            ArgumentNullException.ThrowIfNull(size, "Size");
+            ArgumentNullException.ThrowIfNull(position, nameof(position));
+            ArgumentNullException.ThrowIfNull(size, nameof(size));
 
             var positionSplit = position.Split(",").Select(i => i.Trim()).ToArray();
-            if (positionSplit.Length != 3) throw new ArgumentException("Position must be defined by three parameters", "Position");
+            if (positionSplit.Length != 3) throw new ArgumentException("Position must be defined by three parameters", nameof(position));
 
             this.ChangePosition(positionSplit[0], positionSplit[1], positionSplit[2]);
 
             var sizeSplit = size.Split(",").Select(i => i.Trim()).ToArray();
-            if (sizeSplit.Length < 1) throw new ArgumentMissingException("Size must be defined by at least one parameter", "Size");
-            if (sizeSplit.Length == 2) throw new ArgumentMissingException("Size cannot be defined by only two parameters", "Size");
+            if (sizeSplit.Length < 1) throw new ArgumentMissingException("Size must be defined by at least one parameter", nameof(size));
+            if (sizeSplit.Length == 2) throw new ArgumentMissingException("Size cannot be defined by only two parameters", nameof(size));
 
             if (size.Length == 1) this.ChangeSize(sizeSplit[0], sizeSplit[0], sizeSplit[0]);
             else this.ChangeSize(sizeSplit[0], sizeSplit[1], sizeSplit[2]);
@@ -34,7 +34,14 @@ namespace CubeIntersection.Core.Entities
             this.ChangeSize(width, height, depth);
         }
 
-        private void ChangePosition(string sX, string sY, string sZ)
+        /// <summary>
+        /// Method to change body's position using string values. Those values are expected to be able to be parsed to int
+        /// </summary>
+        /// <param name="sX">Body's position at X axis</param>
+        /// <param name="sY">Body's position at Y axis</param>
+        /// <param name="sZ">Body's position at Z axis</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if any parameter cannot be parsed to int</exception>
+        public void ChangePosition(string sX, string sY, string sZ)
         {
             if (!int.TryParse(sX, out int iX)) throw new ArgumentOutOfRangeException(nameof(Body.X), "X is not an accepted value");
             if (!int.TryParse(sY, out int iY)) throw new ArgumentOutOfRangeException(nameof(Body.Y), "Y is not an accepted value");
@@ -43,14 +50,27 @@ namespace CubeIntersection.Core.Entities
             this.ChangePosition(iX, iY, iZ);
         }
 
-        private void ChangePosition(int x, int y, int z)
+        /// <summary>
+        /// Method to change body's position
+        /// </summary>
+        /// <param name="x">Body's position at X axis</param>
+        /// <param name="y">Body's position at Y axis</param>
+        /// <param name="z">Body's position at Z axis</param>
+        public void ChangePosition(int x, int y, int z)
         {
             this.X = x;
             this.Y = y;
             this.Z = z;
         }
 
-        private void ChangeSize(string sWidth, string sHeight, string sDepth)
+        /// <summary>
+        /// Method to change body's size using string values. Those values are expected to be able to be parsed to float
+        /// </summary>
+        /// <param name="sWidth">Body's width</param>
+        /// <param name="sHeight">Body's height</param>
+        /// <param name="sDepth">Body's depth</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if any parameter cannot be parsed to float</exception>
+        public void ChangeSize(string sWidth, string sHeight, string sDepth)
         {
             if (!float.TryParse(sWidth, out float iWidth)) throw new ArgumentOutOfRangeException(nameof(Body.Width), "Width is not an accepted value");
             if (!float.TryParse(sHeight, out float iHeight)) throw new ArgumentOutOfRangeException(nameof(Body.Height), "Height is not an accepted value");
@@ -59,7 +79,14 @@ namespace CubeIntersection.Core.Entities
             this.ChangeSize(iWidth, iHeight, iDepth);
         }
 
-        private void ChangeSize(float width, float height, float depth)
+        /// <summary>
+        /// Method to change body's size
+        /// </summary>
+        /// <param name="width">Body's width</param>
+        /// <param name="height">Body's height</param>
+        /// <param name="depth">Body's depth</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if any parameter is negative</exception>
+        public void ChangeSize(float width, float height, float depth)
         {
             if (width < 0) throw new ArgumentOutOfRangeException(nameof(Body.Width), "Width cannot be negative");
             if (height < 0) throw new ArgumentOutOfRangeException(nameof(Body.Height), "Height cannot be negative");
