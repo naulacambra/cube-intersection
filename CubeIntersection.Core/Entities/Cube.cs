fd0f9561ab3d4b37a8f1e7a5026566dc3d5ddae1
Arnau Lacambra <naulacambra@gmail.com>
2022-06-02 17:48:38 +0200

    Defined Cube as body collider

---
+++ CubeIntersection.Core/Entities/Cube.cs
@@ -0,0 +1,13 @@
+﻿using CubeIntersection.Core.Entities.Base;
+using CubeIntersection.Core.Interfaces;
+
+namespace CubeIntersection.Core.Entities
+{
+    internal class Cube : BodyBase, ICollider
+    {
+        public bool Collides(BodyBase body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
