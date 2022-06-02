4e572b86347f8505005c35cb2199dab01a4d7035
Arnau Lacambra <naulacambra@gmail.com>
2022-06-02 18:00:14 +0200

    Created ArgumentMissingException

---
+++ CubeIntersection.Core/Exceptions/ArgumentMissingException.cs
@@ -0,0 +1,10 @@
+﻿namespace CubeIntersection.Core.Exceptions
+{
+    public class ArgumentMissingException : ArgumentException
+    {
+        public ArgumentMissingException(string message) : base(message)
+        {
+
+        }
+    }
+}
