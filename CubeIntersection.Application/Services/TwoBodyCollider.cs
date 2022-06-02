6726460b2674e446a88db205860a01d7457b6023
Arnau Lacambra <naulacambra@gmail.com>
2022-06-02 18:19:32 +0200

    Created a TwoBodyCollider

---
+++ CubeIntersection.Application/Services/TwoBodyCollider.cs
@@ -0,0 +1,34 @@
+﻿using CubeIntersection.Application.Exceptions;
+using CubeIntersection.Application.Interfaces;
+using CubeIntersection.Core.Interfaces;
+
+namespace CubeIntersection.Application.Services
+{
+    /// <summary>
+    /// Class to manage two bodies, and see how they interact with each other
+    /// </summary>
+    public class TwoBodyCollider : ICollider
+    {
+        public TwoBodyCollider(IBodyCollider a, IBodyCollider b)
+        {
+            this.A = a;
+            this.B = b;
+        }
+
+        public IBodyCollider A { get; set; }
+
+        public IBodyCollider B { get; set; }
+
+        public bool ValidateBodies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Collides()
+        {
+            if (!this.ValidateBodies()) throw new NonValidBodyException();
+
+            throw new NotImplementedException();
+        }
+    }
+}
